3d3c9b84d8df31b8b160ee0f42cc69596afc96d5
Alex <AlexiyKami@github.com>
2022-02-03 17:55:53 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBracket = [];
+  let closeBracket = [];
+  let arr = [];
+  for (let x of bracketsConfig) {
+    openBracket.push(x[0]);
+    closeBracket.push(x[1]);
+  }
+  for (let symb of str) {
+    let brackets = bracketsConfig.find((pair) => pair[0] === symb || pair[1] === symb);
+    if (closeBracket.includes(symb) && arr[arr.length - 1] === brackets[0]) {
+      arr.pop();
+    } else if (openBracket.includes(symb)) {
+      arr.push(symb);
+    } else {
+      return false;
+    }
+  }
+  return arr.length === 0;
 }
